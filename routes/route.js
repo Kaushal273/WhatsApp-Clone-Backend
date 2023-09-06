016dbd8b1cbafd7270b299ee7a3b51b60ecd5e59
Kaushal273 <kaushalyadav536@gmail.com>
2023-09-06 21:21:05 +0530

    getUser

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { addUser } from '../controller/userController.js';
+import { addUser, getUsers } from '../controller/userController.js';
 
 const route = express.Router();
 
 route.post('/add', addUser);
+route.get('/users', getUsers);
 
 export default route;
